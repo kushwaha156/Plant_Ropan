c140254d10b54ebadcf69bb89cb8149eadb6db40
Ajeet Kushwaha <ajeetkushwaha957@gmail.com>
2024-09-19 06:43:27 -0400

    plans Added

--- src/main/java/com/plant/controller/PlansAdd.java
+++ src/main/java/com/plant/controller/PlansAdd.java
@@ -1,15 +1,21 @@
 package com.plant.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.plant.Dao.CustomerDao;
 import com.plant.Dao.userDao;
+import com.plant.entities.AgentMain;
 import com.plant.entities.Plans;
 
 @Controller
@@ -17,7 +23,10 @@ public class PlansAdd {
 	
 	 @Autowired
 	 userDao userdao;
-	
+	 
+	 @Autowired
+	 private CustomerDao customerDao;
+	 
 	 @PostMapping("/addPlans")
 	 @ResponseBody
 	 public Map<String, String> addPlan(@ModelAttribute Plans plans) {
@@ -32,4 +41,32 @@ public class PlansAdd {
 	     return response;
 	 }
 	
+	 
+	 @GetMapping("/getPlans")
+		public ResponseEntity<HashMap<String, Object>> getAllPlans() {
+		    HashMap<String, Object> response = new HashMap<>();
+		    List<Plans> getPlans = this.customerDao.getallPlans();
+		    List<Plans> activePlans = new ArrayList<>();
+		    for (Plans pl : getPlans) {
+		        if (pl.getIsActive().equals("Yes")) {
+		            activePlans.add(pl);
+		        }
+		    }
+		    response.put("All Plans", activePlans);
+		    return ResponseEntity.ok(response);
+		}
+		
+		@GetMapping("/monthWiseRecordFetch")
+		@ResponseBody
+		public List<Plans> monthWiseRecordFetch() {
+			List<Plans> getmonthPlans= this.customerDao.getMonthVSDailyfetch("MONTHLY");
+			return getmonthPlans;
+		}
+		
+		@GetMapping("/dailyRecordFetch")
+		@ResponseBody
+		public List<Plans> dailyRecordFetch() {
+			List<Plans> getdailyPlans= this.customerDao.getMonthVSDailyfetch("DAILY");
+			return getdailyPlans;
+		}
 }
